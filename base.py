25e63c28d1f48f1002f327f5a42f38eeb6b3e1df
Zach Abbey <zachariahabbey@gmail.com>
2024-03-04 16:53:18 -0600

    Added Nav Bar to most pages with data on them

--- base.py
+++ base.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from excelsheetscraper import scrape_mileage_sheet
 from AthleteSchedule import AthleteSchedule
 from datetime import datetime
@@ -15,6 +15,12 @@ for row in data:
 TEMPLATES = ['mon.html', 'tue.html', 'wed.html', 'thu.html', 'fri.html', 'sat.html', 'sun.html']
 DOW = datetime.today().weekday()
 
+def fixName(name):
+    brokenName = name.split()
+    firstName = brokenName[0]
+    lastName = brokenName[1]
+    return lastName + ', ' + firstName
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -25,6 +31,19 @@ def mileage():
     return f"""<p>This page shows milage!</p>
     {mileage}
     """
+@app.route("/<athleteName>/<int:dow>")
+def weekday(athleteName, dow):
+    new_name = fixName(athleteName)
+    schedule = athletes[new_name]
+    return render_template(
+        TEMPLATES[dow], 
+        athletename=schedule.get_name(), 
+        mileage=schedule.get_days_mileage(dow),
+        fms=schedule.get_fms(),
+        notes=schedule.get_notes(),
+        total_miles=schedule.get_total_miles(),
+        core=schedule.get_core(dow)
+        )
 
 @app.route("/search", methods=['POST'])
 def show_athlete():
@@ -41,4 +60,3 @@ def show_athlete():
             total_miles=schedule.get_total_miles(),
             core=schedule.get_core(DOW)
             )
-
